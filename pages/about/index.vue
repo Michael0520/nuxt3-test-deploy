ac60a5dedb25574c0fdc194894424dbbd082c607
Michael Lo <michael.lo@buyandship.com.tw>
2023-12-20 10:36:15 +0800

    Add new pages: About, Cell, Demo, List, and Zebra

---
+++ pages/about/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <h1 class=" text-balance text-blue">About</h1>
+    <img src='../../public/vite.png' alt="">
+  </div>
+</template>
---
+++ pages/list/index.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <h1>List Page</h1>
+  </div>
+</template>
